b50a77370eeeebf1084f8b87dd510bb13fe4ad27
Amit Bhattacharjee <amitbhattacharjee.3323@gmail.com>
2020-05-16 20:35:47 +0530

    added

---
+++ week2/Assignment/Extracting_data_from_regex.py
@@ -0,0 +1,13 @@
+import re
+
+if __name__ == '__main__':
+    file = open('regex_sum_544321.txt')
+    sum = 0
+    for line in file:
+        temp = line.rstrip()
+        temp = re.findall('[0-9]+', temp)
+        if len(temp) > 0:
+            for w in temp:
+                sum += int(w)
+
+    print('The sum for the sample text above is %d\n' % sum)
---
+++ week4/Quiz/Reading Web-Data-From-Python.md
@@ -0,0 +1,3 @@
+Q1. Which of the following Python data structures is most similar to the value returned in this line of Python:
+* `x = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')`
+Ans : file handle¶
